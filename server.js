b02a3a751f633efdb73d0fcc9ca44d9a8c68df40
Vignesh SB <92362676+admin-402@users.noreply.github.com>
2023-08-06 11:46:48 +0530

    added multi client support

--- server.js
+++ server.js
@@ -15,26 +15,44 @@ const wsServer = new websocket.server({
   httpServer: server
 });
 
+const connections = []; // Array to store all active WebSocket connections
+
 wsServer.on('request', (request) => {
   const connection = request.accept(null, request.origin);
-
   console.log('WebSocket connection accepted');
+  connections.push(connection); // Store the new connection
 
-  const { spawn } = require('child_process');
-  const child = spawn('node', ['receive.js']); // Replace 'your_script_name.js' with the name of your Node.js script file.
-
-  child.stdout.on('data', (data) => {
-    console.log(`Received data from script: ${data}`);
-    connection.sendUTF(data); // Send data to the WebSocket client
+  connection.on('close', () => {
+    console.log('WebSocket connection closed');
+    const index = connections.indexOf(connection);
+    if (index !== -1) {
+      connections.splice(index, 1); // Remove the closed connection from the array
+    }
   });
+});
 
-  child.stderr.on('data', (data) => {
-    console.error(`Error from script: ${data}`);
-  });
+const { spawn } = require('child_process');
+const child = spawn('node', ['receive.js']); // Replace 'your_script_name.js' with the name of your Node.js script file.
 
-  child.on('close', (code) => {
-    console.log(`Child process exited with code ${code}`);
+child.stdout.on('data', (data) => {
+  console.log(`Received data from script: ${data}`);
+  const message = data.toString(); // Convert the data to a string
+  sendToAll(message); // Send data to all WebSocket clients
+});
+
+child.stderr.on('data', (data) => {
+  console.error(`Error from script: ${data}`);
+});
+
+child.on('close', (code) => {
+  console.log(`Child process exited with code ${code}`);
+  connections.forEach((connection) => {
     connection.close();
   });
 });
 
+function sendToAll(message) {
+  connections.forEach((connection) => {
+    connection.sendUTF(message);
+  });
+}
